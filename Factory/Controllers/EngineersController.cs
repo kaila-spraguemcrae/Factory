7a0798b6e2bc9400bf18488fb36a58b030f688f0
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-08 13:27:01 -0800

    add details view and controller for engineer

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -20,5 +20,13 @@ namespace Factory.Controllers
       engineerList.Sort((x, y) => string.Compare(x.LastName, y.LastName));
       return View(engineerList);
     }
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+        .Include(engineer => engineer.Machines)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(engineer => engineer.EngineerId == id); 
+      return View(thisEngineer);
+    }
   }
 }
