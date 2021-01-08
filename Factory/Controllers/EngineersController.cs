b2b257cb3e506bf50b5500998456a9ae33d763e9
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-08 10:35:13 -0800

    add Index controller for Engineeer

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -9,6 +9,16 @@ namespace Factory.Controllers
 {
   public class EngineersController : Controller
   {
-    
+    private readonly FactoryContext _db;
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Engineer> engineerList = _db.Engineers.ToList();
+      engineerList.Sort((x, y) => string.Compare(x.LastName, y.LastName));
+      return View(engineerList);
+    }
   }
 }
