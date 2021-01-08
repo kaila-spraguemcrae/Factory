ac7cc0d85c19d53a33ea1369f4405a8c00a1bad3
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-08 09:52:38 -0800

    add HomeController

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
